d7d836b6c2750740e0354519f7f4fa822ca0bf99
VB <djkoo2001@gmail.com>
2019-03-11 09:05:45 +0700

    bench

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Material;
 
 class IndexController extends Controller
 {
     public function index() {
-        $materials = Material::where('deleted_at', 0)->orderBy('created_at','desc')->Paginate(20);
+        //$materials = Material::where('deleted_at', 0)->orderBy('created_at','desc')->Paginate(20);
+        $materials = $this->getMaterials();
         return view('index', ['materials'=>$materials]);
+    }
+    public function getMaterials()
+    {
+        $materials = Material::where('deleted_at', 0)->orderBy('created_at','desc')->Paginate(20);
+        return $materials;
     }
 }
